216ecf02706801a0a9cba8b0514b899f83d11e5a
adityawintech <aditya#19AI>
2025-03-03 22:32:07 +0530

    Aditya

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,6 +1,14 @@
 export async function GET() {
-    const pages = ["/", "/about", "/contact"];
     const baseUrl = "https://webitya.com";
+    
+    // Define your pages
+    const pages = [
+      "",
+      "about",
+      "services",
+      "contact",
+      "blog"
+    ];
   
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -8,9 +16,9 @@ export async function GET() {
         .map(
           (page) => `
         <url>
-          <loc>${baseUrl}${page}</loc>
+          <loc>${baseUrl}/${page}</loc>
           <lastmod>${new Date().toISOString()}</lastmod>
-          <priority>0.8</priority>
+          <priority>${page === "" ? "1.0" : "0.8"}</priority>
         </url>
       `
         )
@@ -18,7 +26,9 @@ export async function GET() {
     </urlset>`;
   
     return new Response(sitemap, {
-      headers: { "Content-Type": "application/xml" },
+      headers: {
+        "Content-Type": "application/xml",
+      },
     });
   }
   
